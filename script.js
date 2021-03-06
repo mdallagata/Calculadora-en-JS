c001d99638b38be359520b13326e1236947e57c5
mdallagata <mauriciodallagata@gmail.com>
2021-03-06 12:31:41 -0300

    todo lo hecho

---
+++ script.js
@@ -0,0 +1,7 @@
+var num1 = window.prompt("Ingrese el primer número");
+var num2 = window.prompt("Ingrese el segundo número");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+document.write(num1 + num2);
