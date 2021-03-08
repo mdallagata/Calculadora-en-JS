f948313b3f42871e79607445271f3b450adb3883
Mauricio Dall'Agata <mauriciodallagata@gmail.com>
2021-03-08 00:50:11 -0300

    dot added

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,36 @@
 const NUM0 = document
   .getElementById("0")
-  .addEventListener("click", () => clickNumero(0));
+  .addEventListener("click", () => clickBtn(0));
 const NUM1 = document
   .getElementById("1")
-  .addEventListener("click", () => clickNumero(1));
+  .addEventListener("click", () => clickBtn(1));
 const NUM2 = document
   .getElementById("2")
-  .addEventListener("click", () => clickNumero(2));
+  .addEventListener("click", () => clickBtn(2));
 const NUM3 = document
   .getElementById("3")
-  .addEventListener("click", () => clickNumero(3));
+  .addEventListener("click", () => clickBtn(3));
 const NUM4 = document
   .getElementById("4")
-  .addEventListener("click", () => clickNumero(4));
+  .addEventListener("click", () => clickBtn(4));
 const NUM5 = document
   .getElementById("5")
-  .addEventListener("click", () => clickNumero(5));
+  .addEventListener("click", () => clickBtn(5));
 const NUM6 = document
   .getElementById("6")
-  .addEventListener("click", () => clickNumero(6));
+  .addEventListener("click", () => clickBtn(6));
 const NUM7 = document
   .getElementById("7")
-  .addEventListener("click", () => clickNumero(7));
+  .addEventListener("click", () => clickBtn(7));
 const NUM8 = document
   .getElementById("8")
-  .addEventListener("click", () => clickNumero(8));
+  .addEventListener("click", () => clickBtn(8));
 const NUM9 = document
   .getElementById("9")
-  .addEventListener("click", () => clickNumero(9));
+  .addEventListener("click", () => clickBtn(9));
+const DOT = document
+  .getElementById(".")
+  .addEventListener("click", () => clickBtn("."));
 const SUM = document
   .getElementById("+")
   .addEventListener("click", () => asignarOperacion("+"));
@@ -37,6 +40,9 @@ const REST = document
 const MULT = document
   .getElementById("*")
   .addEventListener("click", () => asignarOperacion("*"));
+const DIVIDE = document
+  .getElementById("/")
+  .addEventListener("click", () => asignarOperacion("/"));
 const C = document
   .getElementById("c")
   .addEventListener("click", () => limpiar());
@@ -49,15 +55,15 @@ let numero2;
 let operacion;
 let caja = document.getElementById("resultado");
 
-function clickNumero(numero) {
+function clickBtn(valor) {
   if (caja.innerHTML === "0") {
     caja.innerHTML = "";
   }
-  caja.innerHTML += numero;
+  caja.innerHTML += valor;
 }
 
 function asignarOperacion(sender) {
-  numero1 = parseInt(caja.innerHTML);
+  numero1 = parseFloat(caja.innerHTML);
   caja.innerHTML = "";
   operacion = sender;
   //caja.innerHTML = operacion;
@@ -70,7 +76,7 @@ function limpiar() {
 }
 
 function calcularResultado() {
-  numero2 = parseInt(caja.innerHTML);
+  numero2 = parseFloat(caja.innerHTML);
   console.log(numero1, numero2);
   switch (operacion) {
     case "+": {
@@ -81,14 +87,15 @@ function calcularResultado() {
       caja.innerHTML = numero1 - numero2;
       break;
     }
-    case "/": {
-      caja.innerHTML = numero1 / numero2;
-      break;
-    }
     case "*": {
       caja.innerHTML = numero1 * numero2;
       break;
     }
+    case "/": {
+      caja.innerHTML = numero1 / numero2;
+      break;
+    }
+
     default: {
       alert("xd");
     }
